3f720c458f89053496152bf2bdd84b235e8c54c0
Sushil Pratap Bharati <sushil.bharati007@gmail.com>
2017-02-20 23:37:24 -0600

    Delete credentials.py

--- credentials.py
+++
@@ -1,6 +0,0 @@
-#Please use your own credentials - This was for demo purpose
-
-account_sid="ACea10bf41c7414316649d77b382c78e1f"
-auth_token="c30da31a1e68895383758fb61cc81a96"
-my_cell="+17859799336"
-my_twilio="+19132986214"
